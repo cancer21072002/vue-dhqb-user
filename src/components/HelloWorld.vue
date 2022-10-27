7379f32a5d082a3bd2aee1c2d952d201befcda40
cancer21072002 <92455145+cancer21072002@users.noreply.github.com>
2022-10-27 10:19:16 +0700

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,141 @@
+<template>
+  <div>
+    <H1>LIST USERS</H1>
+
+    <signupUser :edit="userEdit" @OnSubmit="clickAdd"/>
+    <table>
+      <tr>
+        <th>ID</th>
+        <th>Your name</th>
+        <th>Age</th>
+        <th>Phone</th>
+        <th>Email</th>
+        <th>Gender</th>
+        <th>Date</th>
+        <th>Address</th>
+        <th>Pass</th>
+        <th>Action</th>
+    
+      </tr>
+      <tr v-for="(item, index) in students" :key="index">
+        <td>{{ item.id }}</td>
+        <td>{{ item.name }}</td>
+        <td>{{ item.age }}</td>
+        <td>{{ item.phone }}</td>
+        <td>{{ item.email }}</td>
+        <td>{{ item.gender }}</td>
+        <td>{{ item.date }}</td>
+        <td>{{ item.address }}</td>
+        <td>{{item.pass}}</td>
+        <td>
+          <button v-b-modal.modal-prevent-closing @click="clickEdit(item)">Edit</button>
+          <button @click="clickDelete(item)">Delete</button>
+        </td>
+      </tr>
+    
+    </table>
+  </div>
+</template>
+
+<script >
+import signupUser from "./signupUser.vue"
+export default {
+  components: {
+    signupUser
+  },
+
+  data () {
+    return {
+      userEdit:{},
+      students: [
+        {
+          id:1,
+          name: "Shouwa Genroku Rakugo Shinjuu",
+          age: 12,
+          phone: 9839988,
+          email: "a24@gmail.com",
+          gender: "Nam",
+          date: "21/06/1999",
+          address: "nha so 07, Van Phúc , ha Đong",
+          pass:"a123"
+        },
+        {
+          id: 2,
+          name: "Shouwa Genroku Rakugo Shinjuu",
+          age: 12,
+          phone: 9839988,
+          email: "a24@gmail.com",
+          pass: "a123",
+          gender: "Nam",
+          date: "21/06/1999",
+          address: "nha so 07, Van Phúc , ha Đong"
+        },
+        {
+          id: 3,
+          name: "Shouwa Genroku Rakugo Shinjuu",
+          age: 12,
+          phone: 9839988,
+          pass: "a123",
+          email: "a24@gmail.com",
+          gender: "Nữ",
+          date: "21/06/1999",
+          address: "nha so 07, Van Phúc , ha Đong"
+        },
+        {
+          id: 4,
+          name: "Shouwa Genroku Rakugo Shinjuu",
+          age: 12,
+          pass: "a123",
+          phone: 9839988,
+          email: "a24@gmail.com",
+          gender: "Nữ",
+          date: "21/06/1999",
+          address: "nha so 07, Van Phúc , ha Đong"
+        },
+        
+
+      ],
+      methods:{
+        clickAdd(item){
+          this.students.push(item)
+        },
+
+        clickDelete(itemDelete){
+          console.log(itemDelete);
+          for(let i=0;i<this.students.legth;i++){
+            if (itemDelete.id===this.students[i].id){
+              this.students.splice(i,1)
+            }
+          }
+        },
+        clickEdit(itemEdit){
+          this.userEdit = itemEdit;
+          console.log(itemEdit);
+        }
+      }
+
+    }
+  },
+}
+</script>
+
+<style >
+table {
+  font-family: arial, sans-serif;
+  border-collapse: collapse;
+  width: 100%;
+}
+th{
+  background-color: #0e81f5;
+  
+}
+
+td,
+th {
+  border: 4px solid #070707;
+  text-align: left;
+  padding: 15px;
+}
+
+
+</style>
---
+++ src/components/signupUser.vue
@@ -0,0 +1,138 @@
+<template>
+    <div>
+        <b-button v-b-modal.modal-prevent-closing>Signup</b-button>
+       
+
+        <b-modal id="modal-prevent-closing" ref="modal" title="SignUp" @show="resetModal" @hidden="resetModal"
+            @ok="handleOk">
+            <form ref="form" @submit.stop.prevent="handleSubmit">
+
+                <b-form-group label="Name">
+                    <b-form-input  v-model="user.name"  required></b-form-input>
+                </b-form-group>
+
+                <b-form-group label="Age" >
+                    <b-form-input  v-model="user.age"  required></b-form-input>
+                </b-form-group>
+
+                <b-form-group label="Password">
+                    <b-form-input v-model="user.pass" required></b-form-input>
+                </b-form-group>
+
+                <b-form-group label="Phone" >
+                    <b-form-input  v-model="user.phone"  required></b-form-input>
+                </b-form-group>
+
+                <b-form-group label="Email" >
+                    <b-form-input  v-model="user.email"  required></b-form-input>
+                </b-form-group>
+
+                <b-form-group label="Date" >
+                    <b-form-input  v-model="user.date"  type="date" required></b-form-input>
+                </b-form-group>
+
+                <b-form-group label="Gender" >
+                <select id="gender" v-model="user.gender">
+                    <option >Men</option>
+                    <option >Women</option>
+                
+                </select>
+                </b-form-group>
+
+                <b-form-group label="Address" >
+                    <b-form-input  v-model="user.address"  required></b-form-input>
+                </b-form-group>
+
+                
+            </form>
+        </b-modal>
+    </div>
+</template>
+
+<script>
+export default {
+    props:{
+        edit:{
+            type:Object,
+            default:null
+        }
+    },
+    watch:{
+        edit(){
+            if(this.edit){
+                this.user=Object.assign({},this.edit)
+            }else{
+                this.user={}
+            }
+        }
+    },
+    data () {
+        return {
+            user:{
+                id:Math.floor(Math.random()*1000),
+                name: '',
+                age: '',
+                email: '',
+                phone: '',
+                gender: '',
+                
+                address: '',
+                date: '',
+                pass:''
+
+            },
+            name: '',
+            nameState: null,
+            submittedNames: []
+        }
+    },
+    methods: {
+        checkFormValidity () {
+            const valid = this.$refs.form.checkValidity()
+            this.nameState = valid
+            return valid
+        },
+        resetModal () {
+            this.name = ''
+            this.nameState = null
+        },
+        handleOk (bvModalEvent) {
+            
+            console.log(this.user);
+
+            this.$emit("OnSubmit",this.user)
+
+
+            this.user = {
+                id: Math.floor(Math.random() * 1000),
+                name: '',
+                age: '',
+                email: '',
+                phone: '',
+                gender: '',
+
+                address: '',
+                date: '',
+                pass: ''
+            }
+
+            // Prevent modal from closing
+            bvModalEvent.preventDefault()
+            // Trigger submit handler
+            this.handleSubmit()
+        },
+        handleSubmit () {
+            // Exit when the form isn't valid
+            if (!this.checkFormValidity()) {
+                return
+            }
+            // Push the name to submitted names
+            this.submittedNames.push(this.name)
+            // Hide the modal manually
+            this.$nextTick(() => {
+                this.$bvModal.hide('modal-prevent-closing')
+            })
+        }
+    }
+}
+</script>
